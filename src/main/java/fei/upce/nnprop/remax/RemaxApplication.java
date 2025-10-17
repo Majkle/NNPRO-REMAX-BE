33c2c9436e5e7bea8e9777ebffbf2d390355f19d
Michael Pinkas <michael.pinkas@unicorn.com>
2025-10-17 16:21:08 +0200

    spring init

---
+++ src/main/java/fei/upce/nnprop/remax/RemaxApplication.java
@@ -0,0 +1,13 @@
+package fei.upce.nnprop.remax;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RemaxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RemaxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fei/upce/nnprop/remax/RemaxApplicationTests.java
@@ -0,0 +1,13 @@
+package fei.upce.nnprop.remax;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RemaxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
