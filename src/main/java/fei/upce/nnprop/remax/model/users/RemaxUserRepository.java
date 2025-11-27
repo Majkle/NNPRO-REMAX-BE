21010fe3f3c55f5fff1ae70dd3c975dd553e47ba
STAGAJE <93800250+STAGAJE@users.noreply.github.com>
2025-11-27 08:30:21 +0100

    Added default admin

--- src/main/java/fei/upce/nnprop/remax/model/users/RemaxUserRepository.java
+++ src/main/java/fei/upce/nnprop/remax/model/users/RemaxUserRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface RemaxUserRepository extends JpaRepository<RemaxUser, Long> {
+    boolean existsByUsernameOrEmail(String username, String email);
     Optional<RemaxUser> findByUsername(String username);
     Optional<RemaxUser> findByEmail(String email);
 }
---
+++ src/main/java/fei/upce/nnprop/remax/security/config/AdminInitializer.java
@@ -0,0 +1,71 @@
+package fei.upce.nnprop.remax.security.config;
+
+import fei.upce.nnprop.remax.address.AddressService;
+import fei.upce.nnprop.remax.model.realestates.entity.Address;
+import fei.upce.nnprop.remax.model.realestates.enums.AddressRegion;
+import fei.upce.nnprop.remax.model.users.Admin;
+import fei.upce.nnprop.remax.model.users.PersonalInformation;
+import fei.upce.nnprop.remax.model.users.RemaxUserRepository;
+import fei.upce.nnprop.remax.personalInformation.PersonalInformationService;
+import org.springframework.beans.factory.annotation.Value;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.time.OffsetDateTime;
+import java.time.ZonedDateTime;
+
+@Configuration
+@Slf4j
+public class AdminInitializer {
+    @Value("${app.default-admin.username}")
+    private String username;
+
+    private String email = "admin@remax.cz";
+
+    @Value("${app.default-admin.password}")
+    private String password;
+
+    @Bean
+    public ApplicationRunner applicationRunner(
+            RemaxUserRepository userRepository,
+            AddressService addressService,
+            PersonalInformationService personalInformationService,
+            PasswordEncoder passwordEncoder
+    ) {
+        return args -> {
+            if (!userRepository.existsByUsernameOrEmail(username, email)) {
+                log.info("Registering default admin...");
+
+                Address address = new Address();
+                address.setStreet("Československých legií 565");
+                address.setCity("Pardubice");
+                address.setPostalCode("530 02");
+                address.setCountry("Česká republika");
+                address.setRegion(AddressRegion.PARDUBICKY);
+                address = addressService.save(address);
+
+                PersonalInformation pi = new PersonalInformation();
+                pi.setAddress(address);
+                pi.setDegree("Ing.");
+                pi.setFirstName("Jan");
+                pi.setLastName("Novák");
+                pi.setPhoneNumber("+420 321 654 987");
+                pi.setBirthDate(ZonedDateTime.now());
+                pi = personalInformationService.save(pi);
+
+                Admin admin = new Admin();
+                admin.setUsername(username);
+                admin.setEmail(email);
+                admin.setPassword(passwordEncoder.encode(password));
+                admin.setPersonalInformation(pi);
+                admin.setCreatedAt(OffsetDateTime.now());
+
+                userRepository.save(admin);
+                log.info("Registered default admin username={} id={}", admin.getUsername(), admin.getId());
+            }
+        };
+    }
+}
