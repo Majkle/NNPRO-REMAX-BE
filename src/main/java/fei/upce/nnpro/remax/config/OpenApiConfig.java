55db7177a70ccfb9511ef3e7da4408c085a87423
Michael Pinkas <st67089@upce.cz>
2025-12-16 23:18:27 +0100

    Swagger config package fix

--- src/main/java/fei/upce/nnpro/remax/config/OpenApiConfig.java
+++ src/main/java/fei/upce/nnpro/remax/config/OpenApiConfig.java
@@ -1,4 +1,4 @@
-package fei.upce.nnprop.remax.config;
+package fei.upce.nnpro.remax.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
